6a2048912b3b0d7026f86697dce5d908f6f05d4e
iokl1004 <iokl3319@naver.com>
2022-01-20 20:18:42 +0900

    노마드 코더

--- Movie Web/react-for-beginners/src/App.js
+++ Movie Web/react-for-beginners/src/App.js
@@ -1,57 +1,27 @@
-import { getByPlaceholderText } from "@testing-library/react";
-import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image}/>
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres&&movie.genres.map((g) => {g})}
-                {/* {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))} */}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+  return( 
+  <Router>
+    <Switch>
+      <Route path="/hello">
+        <h1>Hello</h1>
+      </Route>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
   );
-
 }
+
 export default App;
-
-//--------------------------------------------------------
-// 본문
-//import { useState, useEffect } from "react";
-
-//function App() {
-//   return <div></div>;
-// }
-
-// export default App;
